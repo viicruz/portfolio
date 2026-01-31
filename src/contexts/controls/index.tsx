05aa996224132bde2a1d3816cb3912bca53b208f
Victor Cruz <VicttorrHugo100@gmail.com>
2026-01-31 14:38:02 -0300

    feat: implement character controls with keyboard input mapping

---
+++ src/contexts/controls/index.tsx
@@ -0,0 +1,33 @@
+import { KeyboardControls, type KeyboardControlsEntry } from "@react-three/drei";
+
+
+export enum Controls {
+  Up = "up",
+  Down = "down",
+  Left = "left",
+  Right = "right",
+  Sprint = "sprint"
+}
+
+const keyboardControls: { name: Controls; keys: string | string[] }[] = [
+  { name: Controls.Up, keys: ["KeyW", "ArrowUp"] },
+  { name: Controls.Down, keys: ["KeyS", "ArrowDown"] },
+  { name: Controls.Left, keys: ["KeyA", "ArrowLeft"] },
+  { name: Controls.Right, keys: ["KeyD", "ArrowRight"] },
+  { name: Controls.Sprint, keys: ["ShiftLeft", "ShiftRight"] },
+];
+
+type KeyboardControlsProps = {
+  children: React.ReactNode;
+}
+
+export function CharacterControls(props: KeyboardControlsProps) {
+  return (
+    <KeyboardControls
+      map={keyboardControls as KeyboardControlsEntry<Controls>[]}
+    >
+      {props.children}
+    </KeyboardControls>
+  );
+
+}

e490cb7a345c9cece9b14c201d2bba44bf0c6135
Victor Cruz <VicttorrHugo100@gmail.com>
2026-02-12 16:29:52 -0300

    feat: remove advance button from dialog component

--- src/components/scene/index.tsx
+++ src/components/scene/index.tsx
@@ -23,13 +23,6 @@ export function Scene(props: SceneProps) {
   return (
     <div className='relative w-full h-svh'>
       <div className='absolute bottom-0 left-0 w-full z-10 flex justify-center pb-8'>
-        <button
-          type='button'
-          onClick={() => {
-            dialogStore.advanceDialog();
-          }} >
-          Avançar
-        </button>
         {dialogStore.isOnDialog && (
           <div className='items-center justify-cente w-full max-w-5xl border py-1 px-2 rounded-2xl bg-black/50 pr-8 relative'>
             <div className='bg-white h-28 rounded-xl px-2 font-pixel text-2xl flex items-center '>

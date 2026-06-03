7308b666d061164ebd205d8ef6dc79ff5bd05170
Victor Cruz <79809602+viicruz@users.noreply.github.com>
2026-06-03 14:57:35 -0300

    Update src/app/page.tsx
    Co-authored-by: Henrique Fonseca <60244227+Tamicktom@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
         </mesh>
         <CharacterControls>
           <Player playerBodyRef={playerBodyRef} />
-          <FollowerPkm size={1} scale={[1, 0.8, 1]} playerBodyRef={playerBodyRef} />
+          <FollowerPkm scale={[1, 0.8, 1]} playerBodyRef={playerBodyRef} />
         </CharacterControls>
         <Npc npcId="npc1" dialogId="dialog1" />
 
